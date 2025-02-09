e501c2178ca4d222624c0702ab3995839ff88126
ekindam <ekindam@gmail.com>
2025-02-08 18:31:19 -0600

    env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   console.log(process.env.API_KEY);
+  const name = process.env.API_KEY
   
   return (
     <main className='container'>
       <h1>Home1</h1>
+      <h1>{name}</h1>
     </main>
   );
 }
