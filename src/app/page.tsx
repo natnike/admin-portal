a98e806176c6413f6c01bef7e336dc11fba03e12
ekindam <ekindam@gmail.com>
2025-02-09 11:21:09 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@
 
 export default function Home() {
   console.log(process.env.API_KEY);
-  const name = process.env.API_KEY
+  const apikey = process.env.API_KEY
+  const browser = process.env.NEXT_PUBLIC_BROWSER_VARIABLE
+  const runtime = process.env.RUNTIME_VARIABLE
   //const name2 = env('API_KEY');
   return (
     <main className='container'>
       <h1>Home1</h1>
-      <h1>{name} DD</h1>
+      <h1>apikey: {process.env.API_KEY}</h1>
+      <h1>BROWSER_VARIABLE: {process.env.NEXT_PUBLIC_BROWSER_VARIABLE}</h1>
+      <h1>RUNTIME_VARIABLE: {process.env.RUNTIME_VARIABLE}</h1>
     </main>
   );
 }
