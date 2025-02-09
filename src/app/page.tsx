72302f2e371a875d89f72b123cde23e812d0cf2f
ekindam <ekindam@gmail.com>
2025-02-08 20:38:38 -0600

    azure env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 export default function Home() {
   console.log(process.env.API_KEY);
-  const name = process.env.API_KEY
+  const name = process.env.NEXT_PUBLIC_API_KEY
   //const name2 = env('API_KEY');
   return (
     <main className='container'>
