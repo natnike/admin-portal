7fba81e2f105d69da2474836266d9f779f30eb43
ekindam <ekindam@gmail.com>
2025-02-28 08:53:00 -0600

    .env.local

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
+  console.log(process.env.NEXTAUTH_SECRET);
   return (
     <div>
       <Button>Button</Button>
+      <h1>{process.env.ADMIN_PORTAL_API_SECRET}</h1>
     </div>
   );
 }
